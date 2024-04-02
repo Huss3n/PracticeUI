04d4c6175d13c14fd19669a6106f4d3dbb2f0299
Huss3n <aisackhussein@gmail.com>
2024-04-02 15:01:51 +0300

    Project setup

---
+++ PracticeUI/PracticeUI/PracticeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeUIApp.swift
+//  PracticeUI
+//
+//  Created by Muktar Hussein on 02/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PracticeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
